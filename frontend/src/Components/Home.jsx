18fce4b0117b809b40cfaf3d1c6c9c24c5a883a9
Nitin Gojiya <nitingojiya2000@gmail.com>
2025-03-27 16:50:30 +0530

    react demo done vercel  1.3  main final 1.0

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
     <div className="max-w-2xl w-full   p-6 text-center">
-      <h1 className="text-3xl font-bold text-gray-800 mb-4">Welcome to Algo Root</h1>
+      <h1 className="text-3xl font-bold text-gray-800 mb-4 ">Welcome to Xyz pvt LTD</h1>
       <p className="text-gray-600 text-lg mb-6">Grow Your Business with AI-Powered Solutions, Including Intelligent Automation, Data Extraction, Web & App Development, and AR/VR Experiences.</p>
       <a href='/details' className="btn btn-outline btn-success">
         Details Pages
